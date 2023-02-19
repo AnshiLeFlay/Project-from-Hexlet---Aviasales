cb279cd63b90eb8d9788de92ebc8351fc3ca68bd
Anshi LeFlay <necrofallen@gmail.com>
2023-02-19 22:34:08 +0400

    final screen

--- frontend/project-aviasales/src/App.tsx
+++ frontend/project-aviasales/src/App.tsx
@@ -11,6 +11,8 @@ import { checkEmail } from "./services/actions";
 import styles from "./app.module.css";
 import "inter-ui/inter.css";
 import aviasalesLogo from "./images/logo.png";
+import rocketAviasales from "./images/rocket.svg";
+import Button from "./components/Button/Button";
 
 const App = () => {
     const email = useSelector((store) => store.email);
@@ -21,9 +23,25 @@ const App = () => {
 
     const [cardOne, setCardOne] = useState<boolean>(true);
     const [cardSecond, setCardSecond] = useState<boolean>(false);
+    const [shareCount, setShareCount] = useState<number>(0);
+    const [secondStep, setSecondStep] = useState<boolean>(false);
+    const [shareError, setShareError] = useState<string>("");
 
     const btnHandle = () => {
         dispatch(checkEmail(email));
+    };
+
+    const handleTestShare = () => {
+        //для теста - если пользователь нажал на кнопку,
+        //считаем, что он поделился
+        setShareCount(shareCount + 1);
+    };
+
+    const handleShared = () => {
+        if (shareCount > 0) {
+            setSecondStep(true);
+            setShareError("");
+        } else setShareError("Надо поделиться");
     };
 
     useEffect(() => {
@@ -39,55 +57,106 @@ const App = () => {
                 <img src={aviasalesLogo} alt="aviasales-logo" />
                 <span>авиасейлс</span>
             </div>
-            <div>
-                <div
-                    className={`${styles.text} ${styles.text_large} ${styles.text_upper_case}`}
-                >
-                    Все круто! Теперь
-                </div>
-                <div
-                    className={`${styles.text} ${styles.text_large} ${styles.text_gradient} ${styles.text_upper_case}`}
-                >
-                    Выигрывай путешествие
-                </div>
-                <div
-                    className={`${styles.text} ${styles.text_medium} ${styles.m_t_45}`}
-                >
-                    Чтобы участвовать в розыгрыше
-                    <br />
-                    путешествия, оставь актуальную почту
-                    <br />и поделись с друзьями
-                </div>
-            </div>
-            <div className={`${styles.m_t_45} ${styles.wrapper_flex_24}`}>
-                <div className={`${styles.wrapper_half}`}>
-                    <Card
-                        n="1"
-                        active={cardOne}
-                        header="Оставь актуальный email"
-                        btnText="Я оставил"
-                        disabledBtn={
-                            email !== "" && error === "" ? false : true
-                        }
-                        onClickHandle={btnHandle}
-                    >
-                        <Email />
-                    </Card>
-                </div>
-                <div className={`${styles.wrapper_half}`}>
-                    <Card
-                        n="2"
-                        active={cardSecond}
-                        header="Поделись с друзьями"
-                        btnText="Я поделился"
-                    >
-                        <div className={`${styles.wrapper_flex_center} ${styles.wrapper_for_flex_space}`}>
-                            <ShareButton type="facebook" />
-                            <ShareButton type="vk" />
-                            <ShareButton type="twitter" />
-                            <ShareButton type="instagram" />
+            <div className={`${styles.wrapper_flex}`}>
+                <div>
+                    <div>
+                        <div
+                            className={`${styles.text} ${styles.text_large} ${styles.text_upper_case}`}
+                        >
+                            {secondStep
+                                ? "Класс! Теперь ты"
+                                : "Все круто! Теперь"}
                         </div>
-                    </Card>
+                        <div
+                            className={`${styles.text} ${styles.text_large} ${styles.text_gradient} ${styles.text_upper_case}`}
+                        >
+                            {secondStep
+                                ? "Участвуешь в конкурсе"
+                                : "Выигрывай путешествие"}
+                        </div>
+                        <div
+                            className={`${styles.text} ${styles.text_medium} ${styles.m_t_45}`}
+                        >
+                            {secondStep ? (
+                                <>
+                                    Ты прошел все наши карты, но ты всегда
+                                    <br />
+                                    можешь вызвать inDriver по-настоящему, для
+                                    <br />
+                                    этого переходи по ссылке!
+                                </>
+                            ) : (
+                                <>
+                                    Чтобы участвовать в розыгрыше
+                                    <br />
+                                    путешествия, оставь актуальную почту
+                                    <br />и поделись с друзьями
+                                </>
+                            )}
+                        </div>
+                    </div>
+                    {secondStep ? (
+                        <div className={`${styles.wrapper_half} ${styles.m_t_45}`}>
+                            <Button>Пройти игру заново</Button>
+                        </div>
+                    ) : (
+                        <div
+                            className={`${styles.m_t_45} ${styles.wrapper_flex_24}`}
+                        >
+                            <div className={`${styles.wrapper_half}`}>
+                                <Card
+                                    n="1"
+                                    active={cardOne}
+                                    header="Оставь актуальный email"
+                                    btnText="Я оставил"
+                                    disabledBtn={
+                                        email !== "" && error === ""
+                                            ? false
+                                            : true
+                                    }
+                                    onClickHandle={btnHandle}
+                                >
+                                    <Email />
+                                </Card>
+                            </div>
+                            <div className={`${styles.wrapper_half}`}>
+                                <Card
+                                    n="2"
+                                    active={cardSecond}
+                                    header="Поделись с друзьями"
+                                    btnText="Я поделился"
+                                    onClickHandle={handleShared}
+                                >
+                                    <div
+                                        className={`${styles.wrapper_flex_center} ${styles.wrapper_for_flex_space}`}
+                                    >
+                                        <ShareButton
+                                            onClick={handleTestShare}
+                                            type="facebook"
+                                        />
+                                        <ShareButton
+                                            onClick={handleTestShare}
+                                            type="vk"
+                                        />
+                                        <ShareButton
+                                            onClick={handleTestShare}
+                                            type="twitter"
+                                        />
+                                        <ShareButton
+                                            onClick={handleTestShare}
+                                            type="instagram"
+                                        />
+                                    </div>
+                                    <div className={`${styles.error_box}`}>
+                                        &nbsp;{shareError}
+                                    </div>
+                                </Card>
+                            </div>
+                        </div>
+                    )}
+                </div>
+                <div>
+                    <img src={rocketAviasales} alt="rocket" />
                 </div>
             </div>
         </div>
